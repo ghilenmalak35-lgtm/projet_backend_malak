18abdcaeacb6ec6bd0a904a3ef33e061f6af4d48
Ghilenmalak35-LGTM <ghilenmalak35@gmail.com>
2026-03-24 17:47:55 +0100

    vent crud

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from fastapi import FastAPI
 from user.user_controller import router
 from client.client_controller import router
 from catégorie.categorie_controller import router
+from fournisseur.fournisseur_controller import router
+from vent.vent_controller import router
 app = FastAPI()
 app.include_router(router)
